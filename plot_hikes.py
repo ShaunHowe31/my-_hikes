ae6aa0338f99a29dc163e7d1363b2367ad0fc8c9
ShaunHowe31 <shaun.howe94@gmail.com>
2023-07-09 17:32:25 -0400

    Updated point map to color code base on activity. Also added the ability to plot based on a list of selected activities.

--- plot_hikes.py
+++ plot_hikes.py
@@ -46,7 +46,7 @@ def process_gpx_to_df(file_name):
     return gpx_df, points
 
 
-def get_gpx_dirs(in_path):
+def get_gpx_dirs(in_path, activity_list):
     '''
     '''
     
@@ -58,21 +58,23 @@ def get_gpx_dirs(in_path):
     for i in range(len(gpx_dirs)):
         activity = gpx_dirs[i].split('/')[-2]
         
-        ## get the GPX files in each activity directory
-        activity_glob = glob.glob(os.path.join(gpx_dirs[i], '*.gpx'))
-        
-        print(activity)
-        if activity not in gpx_dict:
-            gpx_dict[activity] = {'lat_mean':[], 'lon_mean':[], 'points':[]}
-        
-        ## loop through GPX files and save to a dictionary of activities
-        for file in activity_glob:
-            gpx_df, points = process_gpx_to_df(file)
-            
-            gpx_dict[activity]['lon_mean'].append(gpx_df.Longitude.mean())
-            gpx_dict[activity]['lat_mean'].append(gpx_df.Latitude.mean())
-            gpx_dict[activity]['points'].append(points)
-            
+        ## only plot activities from specified list
+        if activity in activity_list:
+            ## get the GPX files in each activity directory
+            activity_glob = glob.glob(os.path.join(gpx_dirs[i], '*.gpx'))
+
+            print(activity)
+            if activity not in gpx_dict:
+                gpx_dict[activity] = {'lat_mean':[], 'lon_mean':[], 'points':[]}
+
+            ## loop through GPX files and save to a dictionary of activities
+            for file in activity_glob:
+                gpx_df, points = process_gpx_to_df(file)
+
+                gpx_dict[activity]['lon_mean'].append(gpx_df.Longitude.mean())
+                gpx_dict[activity]['lat_mean'].append(gpx_df.Latitude.mean())
+                gpx_dict[activity]['points'].append(points)
+
 
     return gpx_dict
         
@@ -99,11 +101,22 @@ def plot_points_on_map(mymap, gpx_dict, map_name):
     
     ## loop through activity GPX and add activitiy layers to the map
     for act in gpx_dict:
+        if act == 'hike':
+            activity_color = 'orange'
+        elif act == 'kayak':
+            activity_color = 'navy'
+        elif act == 'bike':
+            activity_color = 'black'
+        elif act == 'run':
+            activity_color = 'red'
+        elif act == 'ebike':
+            activity_color = 'yellow'
+            
         ## create activity map
         fg_act = folium.FeatureGroup(name=act)
         
         ## plot activity feature
-        fg_act.add_child(folium.PolyLine(gpx_dict[act]['points'], color='cyan', weight=4.5, opacity=.5))
+        fg_act.add_child(folium.PolyLine(gpx_dict[act]['points'], color=activity_color, weight=4.5, opacity=.5))
         mymap.add_child(fg_act)
         
     folium.LayerControl().add_to(mymap)
@@ -137,14 +150,14 @@ def plot_heatmap(mymap, gpx_dict, map_name):
 
 if __name__ == '__main__':
 
-    gpx_dirs = r'/Users/shaunhowe/Documents/GPS/Strava/activities/*/'
+    gpx_dirs = r'/path/to/strava/activities/*/'
     
-    gpx_dict = get_gpx_dirs(gpx_dirs)
+    gpx_dict = get_gpx_dirs(gpx_dirs, ['hike', 'kayak'])
 
 
     mymap1 = create_map(np.mean(gpx_dict['hike']['lat_mean']), np.mean(gpx_dict['hike']['lon_mean']))
     mymap2 = create_map(np.mean(gpx_dict['hike']['lat_mean']), np.mean(gpx_dict['hike']['lon_mean']))
     
     # plot_on_map(mymap, gpx_dict['hike']['points'], 'mymap.html')
-    plot_points_on_map(mymap1, gpx_dict, 'mymap_points.html')
-    plot_heatmap(mymap2, gpx_dict, 'mymap_heat.html')
+    plot_points_on_map(mymap1, gpx_dict, 'mymap_points_new.html')
+    plot_heatmap(mymap2, gpx_dict, 'mymap_heat_new.html')
