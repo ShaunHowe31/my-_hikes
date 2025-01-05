1e4911dbbad71e487f95a7a942a17e4ac0d7ae71
Shaun Howe <shaunhowe@Shauns-MacBook-Air.local>
2025-01-05 16:15:04 -0500

    Updated Strava activity sorting to deal with new string activity storage and old integer category storage. Fixed some minor code bugs.

--- sort_strava_activities.py
+++ sort_strava_activities.py
@@ -13,6 +13,25 @@ import glob
 import gpxpy
 import shutil
 
+def determine_strava_gpx(initial_gpx):
+    '''
+    Determine if strava GPX files store activity type using the new (string) 
+    or old (number category) method
+    '''
+    
+    initial_act = initial_gpx.tracks[0].type
+    
+    if isinstance(initial_act, str):
+        act_stor = 'new'
+    elif isinstance(initial_act, int):
+        act_stor = 'old'
+        
+    ## Open strava categories YML to create dictionary
+    with open(category_fn) as file:
+        strava_cats = yaml.load(file, Loader=yaml.FullLoader)
+        strava_cats = strava_cats[act_stor]['strava_activity_categories']
+        
+    return act_stor, strava_cats
 
 def create_activity_dirs(activities, act_dir):
     '''
@@ -28,7 +47,7 @@ def create_activity_dirs(activities, act_dir):
             print(new_dir + ' already exists')
     
 
-def get_strava_category(gpxx_file, strava_cats_df):
+def get_strava_category(gpx_file, activity_storage, strava_cats_df):
     '''
     Function for getting the Strava Category (activity) type
     from the GPX file
@@ -40,18 +59,22 @@ def get_strava_category(gpxx_file, strava_cats_df):
 
     activity_type = gpx.tracks[0].type
 
-    ## loop through Strava category dictionary to find the activity type
-    for tpe in strava_cats_df:
-        if tpe is int(activity_type):
-            activity_name = strava_cats_df[tpe]
-            
-    if activity_name == None:
-        print(activity_type + ' not in strava activity YML')
+    if activity_storage == 'new':
+        activity_name = activity_type
+    
+    elif activity_storage == 'old':
+        ## loop through Strava category dictionary to find the activity type
+        for tpe in strava_cats_df:
+            if tpe is int(activity_type):
+                activity_name = strava_cats_df[tpe]
+
+        if activity_name == None:
+            print(activity_type + ' not in strava activity YML')
         
     return activity_name
 
             
-def sort_strava_gpx(gpx_activity, act_dir, gpx_file):
+def sort_strava_gpx(gpx_activity, new_dir, gpx_file):
     '''
     Sort Strava GPX files by activity type
     '''
@@ -59,9 +82,9 @@ def sort_strava_gpx(gpx_activity, act_dir, gpx_file):
     gpx_fn = os.path.basename(gpx_file)
     
     if gpx_activity != None:
-        new_dir = os.path.join(act_dir, gpx_activity)
+        new_dir_final = os.path.join(new_dir, gpx_activity)
         if os.path.exists(new_dir) == True:
-            new_fp = os.path.join(new_dir, gpx_fn)
+            new_fp = os.path.join(new_dir_final, gpx_fn)
             shutil.move(gpx_file, new_fp)
             
             print('Moving: ', new_fp)
@@ -106,28 +129,27 @@ def combine_activities(strava_dir, new_dir):
     
     
 if __name__ == '__main__':
+    
+    orig_dir    = '/path/to/original/dir'
+    act_dir     = rf'{orig_dir}/Activities'
+    new_dir     = r'/path/to/new/dir'
     category_fn = r'./strava_categories.yml'
-
-    ## Open strava categories YML to create dictionary
-    with open(category_fn) as file:
-        strava_cats = yaml.load(file, Loader=yaml.FullLoader)
-        strava_cats = strava_cats['strava_activity_categories']
-
-    act_dir = r'/path/to/new/directory'
     
+    ## Find out which strava activity type we are dealing with
+    gpx_files   = glob.glob(rf'{act_dir}/*.gpx')
+    initial_gpx = gpxpy.parse(open(gpx_files[0]))
     
-    create_activity_dirs(strava_cats, act_dir)
+    activity_storage, strava_cats = determine_strava_gpx(initial_gpx)
     
-    gpx_files = glob.glob(rf'{act_dir}/*.gpx')
+    ## Create activity directories
+    create_activity_dirs(strava_cats, new_dir)
 
-    ## loop through GPX files
+    ## Loop through GPX files
     for gpx_file in gpx_files:
         
-        activity = get_strava_category(gpx_file, strava_cats)
+        activity = get_strava_category(gpx_file, activity_storage, strava_cats)
         
-        sort_strava_gpx(activity, act_dir, gpx_file)
-        
-    
-    orig_strava_dir = r'/path/to/orig/direcotry'
-    
-    combine_activities(orig_strava_dir, act_dir) 
+        sort_strava_gpx(activity, new_dir, gpx_file)
+
+    ## Combine activities
+    combine_activities(orig_dir, new_dir)
